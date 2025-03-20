63e975be5712d495c2a7424e1324646768cedbc1
vicmtn <victorien.montavon@gmail.com>
2025-03-21 00:47:05 +0100

    feat: update resource controllers with admin checks

--- app/controllers/promotion_asserts_controller.rb
+++ app/controllers/promotion_asserts_controller.rb
@@ -1,5 +1,6 @@
 class PromotionAssertsController < ApplicationController
   before_action :set_promotion_assert, only: %i[ show edit update destroy ]
+  before_action :require_admin, except: [:index, :show]
 
   # GET /promotion_asserts or /promotion_asserts.json
   def index
@@ -23,48 +24,36 @@ class PromotionAssertsController < ApplicationController
   def create
     @promotion_assert = PromotionAssert.new(promotion_assert_params)
 
-    respond_to do |format|
-      if @promotion_assert.save
-        format.html { redirect_to @promotion_assert, notice: "Promotion assert was successfully created." }
-        format.json { render :show, status: :created, location: @promotion_assert }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @promotion_assert.errors, status: :unprocessable_entity }
-      end
+    if @promotion_assert.save
+      redirect_to promotion_asserts_path, notice: 'Promotion assert was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /promotion_asserts/1 or /promotion_asserts/1.json
   def update
-    respond_to do |format|
-      if @promotion_assert.update(promotion_assert_params)
-        format.html { redirect_to @promotion_assert, notice: "Promotion assert was successfully updated." }
-        format.json { render :show, status: :ok, location: @promotion_assert }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @promotion_assert.errors, status: :unprocessable_entity }
-      end
+    if @promotion_assert.update(promotion_assert_params)
+      redirect_to promotion_asserts_path, notice: 'Promotion assert was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /promotion_asserts/1 or /promotion_asserts/1.json
   def destroy
-    @promotion_assert.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to promotion_asserts_path, status: :see_other, notice: "Promotion assert was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @promotion_assert.destroy
+    redirect_to promotion_asserts_path, notice: 'Promotion assert was successfully deleted.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_promotion_assert
-      @promotion_assert = PromotionAssert.find(params.expect(:id))
+      @promotion_assert = PromotionAssert.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def promotion_assert_params
-      params.expect(promotion_assert: [ :description, :function, :moment_id, :sector_id ])
+      params.require(:promotion_assert).permit(:name, :description)
     end
 end
--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,6 @@
 class SubjectsController < ApplicationController
-  before_action :set_subject, only: %i[ show edit update destroy ]
+  before_action :set_subject, only: [:show, :edit, :update, :destroy]
+  before_action :require_admin, except: [:index, :show]
 
   # GET /subjects or /subjects.json
   def index
@@ -23,48 +24,36 @@ class SubjectsController < ApplicationController
   def create
     @subject = Subject.new(subject_params)
 
-    respond_to do |format|
-      if @subject.save
-        format.html { redirect_to @subject, notice: "Subject was successfully created." }
-        format.json { render :show, status: :created, location: @subject }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @subject.errors, status: :unprocessable_entity }
-      end
+    if @subject.save
+      redirect_to subjects_path, notice: 'Subject was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /subjects/1 or /subjects/1.json
   def update
-    respond_to do |format|
-      if @subject.update(subject_params)
-        format.html { redirect_to @subject, notice: "Subject was successfully updated." }
-        format.json { render :show, status: :ok, location: @subject }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @subject.errors, status: :unprocessable_entity }
-      end
+    if @subject.update(subject_params)
+      redirect_to subjects_path, notice: 'Subject was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /subjects/1 or /subjects/1.json
   def destroy
-    @subject.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to subjects_path, status: :see_other, notice: "Subject was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @subject.destroy
+    redirect_to subjects_path, notice: 'Subject was successfully deleted.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_subject
-      @subject = Subject.find(params.expect(:id))
+      @subject = Subject.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def subject_params
-      params.expect(subject: [ :slug, :name, :teacher_id ])
+      params.require(:subject).permit(:name, :description, :course_id)
     end
 end
