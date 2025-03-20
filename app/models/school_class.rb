333432ee485f799dbb398dc4c3e2cdf7c3f0a8e1
vicmtn <victorien.montavon@gmail.com>
2025-03-20 19:11:44 +0100

    feat: Update SchoolClass model with master validation and associations

--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -1,6 +1,21 @@
 class SchoolClass < ApplicationRecord
   belongs_to :moment
   belongs_to :room
-  belongs_to :master
+  belongs_to :master, class_name: 'Person'
   belongs_to :sector
+  has_many :students_classes
+  has_many :students, through: :students_classes
+  has_many :courses
+
+  validates :master, presence: true
+  validate :master_must_be_teacher_or_dean
+
+  private
+
+  def master_must_be_teacher_or_dean
+    return if master.nil?
+    unless master.is_a?(Teacher) || master.is_a?(Dean)
+      errors.add(:master, "must be either a teacher or a dean")
+    end
+  end
 end
