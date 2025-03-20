dce315bd77d11a075e4e9d0d146c4200793955fa
vicmtn <victorien.montavon@gmail.com>
2025-03-20 19:12:24 +0100

    refactor: Remove role validation from Teacher model and update current_subjects method to use moments association

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,13 +3,11 @@ class Teacher < Person
   has_many :courses
   has_many :school_classes, through: :courses
 
-  validates :role, inclusion: { in: %w[teacher] }
-
   def current_classes
     school_classes.joins(:moment).where('moments.end_on >= ?', Date.current)
   end
 
   def current_subjects
-    subjects.joins(:courses).where('courses.end_at >= ?', Time.current).distinct
+    subjects.joins(:moment).where('moments.end_on >= ?', Date.current)
   end
 end 
