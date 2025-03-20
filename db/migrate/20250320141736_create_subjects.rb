b5b70996eab1e8f821a103489948dbce8bf7df75
vicmtn <victorien.montavon@gmail.com>
2025-03-20 19:12:49 +0100

    fix: Update foreign key reference for teacher in subjects migration to point to people table

--- db/migrate/20250320141736_create_subjects.rb
+++ db/migrate/20250320141736_create_subjects.rb
@@ -3,7 +3,7 @@ class CreateSubjects < ActiveRecord::Migration[8.0]
     create_table :subjects do |t|
       t.string :slug
       t.string :name
-      t.references :teacher, null: false, foreign_key: true
+      t.references :teacher, null: false, foreign_key: { to_table: :people }
 
       t.timestamps
     end
