0ee6f903b5ea3c3cf81855db8f13bf88e61ba2a7
vicmtn <victorien.montavon@gmail.com>
2025-03-25 09:00:41 +0100

    feat: enhance school classes controller with user authentication

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,9 +1,15 @@
 class SchoolClassesController < ApplicationController
-  before_action :set_school_class, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :set_school_class, only: [:show, :edit, :update, :destroy]
 
   # GET /school_classes or /school_classes.json
   def index
-    @school_classes = SchoolClass.all
+    if user_signed_in? && current_user.person&.type == 'Student'
+      @current_class = current_user.person.current_class
+      @classmates = @current_class.students.order(:lastname)
+    else
+      @school_classes = SchoolClass.all.includes(:moment).order(:name)
+    end
   end
 
   # GET /school_classes/1 or /school_classes/1.json
@@ -23,48 +29,36 @@ class SchoolClassesController < ApplicationController
   def create
     @school_class = SchoolClass.new(school_class_params)
 
-    respond_to do |format|
-      if @school_class.save
-        format.html { redirect_to @school_class, notice: "School class was successfully created." }
-        format.json { render :show, status: :created, location: @school_class }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @school_class.errors, status: :unprocessable_entity }
-      end
+    if @school_class.save
+      redirect_to @school_class, notice: 'School class was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /school_classes/1 or /school_classes/1.json
   def update
-    respond_to do |format|
-      if @school_class.update(school_class_params)
-        format.html { redirect_to @school_class, notice: "School class was successfully updated." }
-        format.json { render :show, status: :ok, location: @school_class }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @school_class.errors, status: :unprocessable_entity }
-      end
+    if @school_class.update(school_class_params)
+      redirect_to @school_class, notice: 'School class was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /school_classes/1 or /school_classes/1.json
   def destroy
-    @school_class.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to school_classes_path, status: :see_other, notice: "School class was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @school_class.destroy
+    redirect_to school_classes_url, notice: 'School class was successfully destroyed.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_school_class
-      @school_class = SchoolClass.find(params.expect(:id))
+      @school_class = SchoolClass.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def school_class_params
-      params.expect(school_class: [ :uid, :name, :moment_id, :room_id, :master_id, :sector_id ])
+      params.require(:school_class).permit(:name, :moment_id)
     end
 end
