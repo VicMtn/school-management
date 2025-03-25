98df24a64f2c417da18393cf5d08a59474d2a8ce
vicmtn <victorien.montavon@gmail.com>
2025-03-25 23:12:49 +0100

    fix: ensure distinct results for current classes and subjects in Teacher model

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -4,10 +4,10 @@ class Teacher < Person
   has_many :school_classes, through: :courses
 
   def current_classes
-    school_classes.joins(:moment).where('moments.end_on >= ?', Date.current)
+    school_classes.joins(:moment).where('moments.end_on >= ?', Date.current).distinct
   end
 
   def current_subjects
-    subjects.joins(:moment).where('moments.end_on >= ?', Date.current)
+    subjects.joins(:courses => :moment).where('moments.end_on >= ?', Date.current).distinct
   end
 end 
