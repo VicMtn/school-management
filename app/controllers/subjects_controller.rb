5e6bf86922b4cd7a9f2c423683af2259ddc11617
vicmtn <victorien.montavon@gmail.com>
2025-03-26 03:21:25 +0100

    feat: optimize subject loading by eager loading teacher data and update view to display teacher's full name

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -63,7 +63,7 @@ class SubjectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_subject
-      @subject = Subject.find(params[:id])
+      @subject = Subject.includes(:teacher).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
