4b2e1dc733a5162716c32cc720d5489fa1c38d28
vicmtn <victorien.montavon@gmail.com>
2025-03-26 03:08:32 +0100

    refactor: streamline admin dashboard navigation by redirecting tabs for Courses and Subjects, and replace hardcoded links with shared partial

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,13 +5,17 @@ module Admin
     def index
       @active_tab = params[:tab] || 'people'
       
+      if @active_tab == 'subjects'
+        redirect_to subjects_path
+        return
+      elsif @active_tab == 'courses'
+        redirect_to courses_path
+        return
+      end
+      
       case @active_tab
       when 'people'
         @resources = Person.all
-      when 'courses'
-        @resources = Course.all
-      when 'subjects'
-        @resources = Subject.all
       when 'promotion_asserts'
         @resources = PromotionAssert.all
       end
