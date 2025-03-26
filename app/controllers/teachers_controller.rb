d09900d3a68318a7c47d27e45cd27d69590e144b
vicmtn <victorien.montavon@gmail.com>
2025-03-26 03:07:29 +0100

    feat: enhance teacher management views with new teacher creation and improved edit functionality, including subject and class associations

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -24,17 +24,46 @@ class TeachersController < ApplicationController
 
   def create
     @teacher = Teacher.new(teacher_params)
+    
     if @teacher.save
+      # Handle subject associations
+      if params[:teacher][:subject_ids].present?
+        @teacher.subjects = Subject.find(params[:teacher][:subject_ids].reject(&:blank?))
+      end
+      
+      # Handle class associations
+      if params[:teacher][:school_class_ids].present?
+        @teacher.school_classes = SchoolClass.find(params[:teacher][:school_class_ids].reject(&:blank?))
+      end
+      
       redirect_to @teacher, notice: 'Teacher was successfully created.'
     else
+      @subjects = Subject.all
+      @school_classes = SchoolClass.includes(:moment).all
       render :new, status: :unprocessable_entity
     end
   end
 
   def update
     if @teacher.update(teacher_params)
+      # Handle subject associations
+      if params[:teacher][:subject_ids].present?
+        @teacher.subjects = Subject.find(params[:teacher][:subject_ids].reject(&:blank?))
+      else
+        @teacher.subjects = []
+      end
+      
+      # Handle class associations
+      if params[:teacher][:school_class_ids].present?
+        @teacher.school_classes = SchoolClass.find(params[:teacher][:school_class_ids].reject(&:blank?))
+      else
+        @teacher.school_classes = []
+      end
+      
       redirect_to @teacher, notice: 'Teacher was successfully updated.'
     else
+      @subjects = Subject.all
+      @school_classes = SchoolClass.includes(:moment).all
       render :edit, status: :unprocessable_entity
     end
   end
