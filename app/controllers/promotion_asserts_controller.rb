4dcb14cfb41ca7adca218ebd2015cc4f3a33ed6e
vicmtn <victorien.montavon@gmail.com>
2025-03-26 03:27:49 +0100

    feat: enhance promotion assert forms by replacing text fields with collection selects for function, moment, and sector, and improve display of selected values in the view

--- app/controllers/promotion_asserts_controller.rb
+++ app/controllers/promotion_asserts_controller.rb
@@ -14,10 +14,12 @@ class PromotionAssertsController < ApplicationController
   # GET /promotion_asserts/new
   def new
     @promotion_assert = PromotionAssert.new
+    load_collections
   end
 
   # GET /promotion_asserts/1/edit
   def edit
+    load_collections
   end
 
   # POST /promotion_asserts or /promotion_asserts.json
@@ -27,6 +29,7 @@ class PromotionAssertsController < ApplicationController
     if @promotion_assert.save
       redirect_to promotion_asserts_path, notice: 'Promotion assert was successfully created.'
     else
+      load_collections
       render :new, status: :unprocessable_entity
     end
   end
@@ -36,6 +39,7 @@ class PromotionAssertsController < ApplicationController
     if @promotion_assert.update(promotion_assert_params)
       redirect_to promotion_asserts_path, notice: 'Promotion assert was successfully updated.'
     else
+      load_collections
       render :edit, status: :unprocessable_entity
     end
   end
@@ -49,11 +53,16 @@ class PromotionAssertsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_promotion_assert
-      @promotion_assert = PromotionAssert.find(params[:id])
+      @promotion_assert = PromotionAssert.includes(:moment, :sector).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def promotion_assert_params
-      params.require(:promotion_assert).permit(:name, :description)
+      params.require(:promotion_assert).permit(:description, :function, :moment_id, :sector_id)
+    end
+    
+    def load_collections
+      @moments = Moment.all.order(start_on: :desc)
+      @sectors = Sector.all.order(:name)
     end
 end
