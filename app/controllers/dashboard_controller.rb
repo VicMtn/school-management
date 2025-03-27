d801289b357d7312475cca8852a7e82af3b83728
vicmtn <victorien.montavon@gmail.com>
2025-03-27 13:44:19 +0100

    feat: Add limit to recent grades shown in student dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
     if user_signed_in? && current_user.person&.type == 'Student'
       @current_class = current_user.person.current_class
       @current_courses = @current_class.courses.includes(:subject, :teacher).order(:start_at)
-      @grades = current_user.person.grades.includes(:examination).order(execution_date: :desc)
+      @grades = current_user.person.grades.includes(:examination).order(execution_date: :desc).limit(5)
     else
       @total_students = Student.count
       @total_teachers = Teacher.count
