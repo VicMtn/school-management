d7158e820556e3008c358f3e97848b0680a5f72b
vicmtn <victorien.montavon@gmail.com>
2025-03-28 01:20:40 +0100

    feat: update examination model and views with validations, improved layouts, and student grade management

--- app/models/examination.rb
+++ app/models/examination.rb
@@ -2,4 +2,10 @@ class Examination < ApplicationRecord
   include SoftDeletable
   
   belongs_to :course
+  has_many :grades
+  has_many :students, through: :grades
+
+  validates :title, presence: true
+  validates :effective_date, presence: true
+  validates :course_id, presence: true
 end
