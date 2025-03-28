918bef55cf3852f88a66f28b8b4db4fe0435bcbb
vicmtn <victorien.montavon@gmail.com>
2025-03-28 03:20:04 +0100

    feat: add soft delete and restore methods to PromotionAssert model

--- app/models/promotion_assert.rb
+++ app/models/promotion_assert.rb
@@ -1,4 +1,15 @@
 class PromotionAssert < ApplicationRecord
   belongs_to :moment
   belongs_to :sector
+  
+  # Soft delete
+  default_scope { where(deleted_at: nil) }
+  
+  def soft_delete
+    update(deleted_at: Time.current)
+  end
+  
+  def restore
+    update(deleted_at: nil)
+  end
 end
