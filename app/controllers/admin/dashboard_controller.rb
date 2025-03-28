1678d30f8a0a730a16ed70a9f98584d8449f5b6d
vicmtn <victorien.montavon@gmail.com>
2025-03-28 02:40:26 +0100

    feat: add redirect for promotion asserts tab in admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -11,13 +11,14 @@ module Admin
       elsif @active_tab == 'courses'
         redirect_to courses_path
         return
+      elsif @active_tab == 'promotion_asserts'
+        redirect_to promotion_asserts_path
+        return
       end
       
       case @active_tab
       when 'people'
         @resources = Person.all
-      when 'promotion_asserts'
-        @resources = PromotionAssert.all
       end
     end
   end
